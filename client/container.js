9da59c252e5aca8f41139e7b5f80e9f08337de50
zreardon <b-zarea@MRX1141.northamerica.corp.microsoft.com>
2014-07-17 12:42:12 -0700

    initial fb integration

--- client/container.js
+++ client/container.js
@@ -1,47 +1,35 @@
 
+var data = [ {"profilePic": "/images/profile.png", "name": "You", "userName": "@ericsson"},
+			{"profilePic": "/images/Friend1.jpg", "name": "Zachary Reardon", "userName": "31 mutual friends"},
+			{"profilePic": "/images/Friend2.jpg", "name": "Mithla Tople", "userName": "15 mutual friends"},
+			{"profilePic": "/images/Friend3.jpg", "name": "Kevin Sung", "userName": "0 mutual friends"},
+			{"profilePic": "/images/Friend4.jpg", "name": "Prita Nigam", "userName": "10 mutual friends"},
+			{"profilePic": "/images/content4.jpg", "name": "This Guy", "userName": "-1 mutual friends"},
+			{"profilePic": "/images/twitter.png", "name": "Twitter", "userName": "1B mutual friends"}
+			]
 
-var container = function(token) {
+var Container = function(token) {
 
-	var self = this;
-	jQuery.ajax( {
-		  url: 'http://127.0.0.1:1337/update/friends?token=' + token, //?service=' + serviceName + '&date_range=YYYY-MM-DD HH:MM:SS.0 YYYY-MM-DD HH:MM:SS.0',
-
-		  contentType: 'application/json',
-		  type: "GET",	  //crossDomain: "true",
-		  //jsonpCallback: 'jsonpCallback',
-		  //dataType: "json",
-		  async: false,
-		  //jsonp: "jsonp",
-
-		  success: function(d) {
-			 console.log('Ajax old get data was a success');
-			 d = JSON.parse(d);
-			 self.addFilmStripItem(d);
-		  },
-
-		  error: function(xhr, status, errthrwn) {
-			 console.log("Ajax call failed " + status + " " + errthrwn);
-			 //console.log(xhr.getResponseHeader());
-		  }
-
-	})
+	this.addFilmStripItem(data);
 }
 
-container.filmStripItemSize = 0;
+Container.filmStripItemSize = 0;
 
-container.prototype.addFilmStripItem = function(d) { 
+Container.prototype.addFilmStripItem = function(d) { 
+console.log(d);
 	d.forEach( function(data, index) {
-		$('#filmStrip').append(	
-		'<a href="#!" class="fixedFlexItem">\
-			<div id="filmStripItem'+ container.filmStripItemSize + '" class="filmStripItem active vertical fixedFlexItem flexContainer" onclick="$("#wrapper").toggleClass("noDisplay");">\
-			<img src="' + data.profilePic +'" class="fixedFlexItem"></img>\
-			<div id="friendContainer'+ container.filmStripItemSize + '" class="friendContainer fixedFlexItem">\
-				<p id="friendName'+ container.filmStripItemSize + '" class="friendName">'+ data.name +'</p>\
-				<p id="friendInfo'+ container.filmStripItemSize + '" class="friendInfo">'+ data.userName +'@ZacharyReardon</p>\
+	console.log(data);
+		$('#filmStripMenu').append(	
+		$('<a href="#!" class="fixedFlexItem">\
+			<div id="filmStripItem'+ Container.filmStripItemSize + '" class="filmStripItem active vertical fixedFlexItem flexContainer" onclick="$("#wrapper").toggleClass("noDisplay");">\
+			<img src="' + data.profilePic +'" class="fixedFlexItem playListImage"></img>\
+			<div id="friendContainer'+ Container.filmStripItemSize + '" class="friendContainer fixedFlexItem">\
+				<p id="friendName'+ Container.filmStripItemSize + '" class="friendName">'+ data.name +'</p>\
+				<p id="friendInfo'+ Container.filmStripItemSize + '" class="friendInfo">'+ data.userName +'</p>\
 			</div>\
 	</div>\
-	</a>');
-	container.filmStripItemSize++;
+	</a>'));
+	Container.filmStripItemSize++;
   }
   ); 
 };
